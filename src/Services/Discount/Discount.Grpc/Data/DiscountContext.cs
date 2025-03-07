23f9d6cd978e91f80e783e5f2d17bee31d1f4b34
Avinash Pawar <AvinashP@centiro.se>
2025-03-07 17:39:39 +0530

    Added code for discount grpc with docker

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,24 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data;
+
+public class DiscountContext :DbContext
+{
+    public DbSet<Coupon> Coupons { get; set; } = default!;
+
+    public DiscountContext(DbContextOptions<DiscountContext> options) 
+        : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coupon>().HasData(
+            new Coupon {Id = 1,ProductName = "IPhone15",Amount = 120000,Description = "Iphone is Good"},
+            new Coupon { Id = 2, ProductName = "Nokia", Amount = 80000, Description = "I like Nokia" });
+    }
+
+}
+
